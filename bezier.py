18cefda3ad44c423d566f1cca3c06d71dbbac74e
Joe Colasurdo <joe@tratrix.lan>
2020-01-18 14:14:37 -0800

    initial commit

---
+++ bezier.py
@@ -0,0 +1,15 @@
+points = [
+    [4 10],
+    [0 180],
+    [90 0],
+    [180 120],
+    [270 60]
+]
+
+n = 10
+
+def main(points, n):
+    print(points, n)
+
+if if __name__ == "__main__":
+    main(points, n)
